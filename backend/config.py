7733e73c6dd24f4fc871fd545613c662f7cb0844
Ethan <dhruvpataudi495@gmail.com>
2026-07-13 17:28:33 +0530

    API_KEY changes

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,6 @@ load_dotenv(dotenv_path=env_path)
 
 QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 VECTOR_NAME = os.getenv("VECTOR_NAME")
